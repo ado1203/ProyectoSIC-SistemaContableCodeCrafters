5a718d9a9d5baf9175a1d9b992a37d4307affd9f
Danield05 <joseaquino971@gmail.com>
2023-10-25 13:46:50 -0600

    fixes to view and adding styles to ledger Co-authored-by: Adonay Gutiérrez <ado1203@users.noreply.github.com>

--- general_ledger/views.py
+++ general_ledger/views.py
@@ -3,6 +3,7 @@ from .forms import AccountForm, TransactionForm, LedgerForm
 from .models import Account, Category, Transaction, Ledger
 from django.shortcuts import get_object_or_404
 
+
 def get_account_order_by_category():
     accounts = Account.objects.all().order_by('category__name', 'code')
 
@@ -10,30 +11,27 @@ def get_account_order_by_category():
     for account in accounts:
         category_name = account.category.name
         if category_name not in categorized_accounts:
-            categorized_accounts[category_name] = {
-                'category': account.category,
-                'accounts': [],
-            }
-        categorized_accounts[category_name]['accounts'].append(account)
+            categorized_accounts[category_name] = []
+        categorized_accounts[category_name].append(account)
 
-    category_order = ["Activo", "Pasivo", "Patrimonio", "Gastos y Costos", "Ingresos"]
+    # Define el orden específico de las categorías
+    category_order = ["Activo", "Pasivo", "Patrimonio", "Gastos y Costos",
+                      "Ingresos"]
 
-    for category_name, data in categorized_accounts.items():
-        category = data['category']
-        accounts = data['accounts']
-        rowspan = len(accounts) + 1
-        category.rowspan = rowspan
+    ordered_accounts = {
+        category_name: categorized_accounts.get(category_name, []) for
+        category_name in category_order}
 
-    ordered_categories = [categorized_accounts.get(category_name) for category_name in category_order]
+    return ordered_accounts
 
-    return ordered_categories
 
 def account(request):
-    ordered_categories = get_account_order_by_category()
+    ordered_accounts = get_account_order_by_category()
 
     return render(request, 'account.html', {
-        'categorized_accounts': ordered_categories
+        'categorized_accounts': ordered_accounts
     })
+
 
 def create_account(request):
     if request.method == 'POST':
@@ -45,7 +43,7 @@ def create_account(request):
     else:
         form = AccountForm()
 
-    categories = Category.objects.all()  # Corrección aquí
+    categories = Category.objects.all()
     return render(request, 'create_account.html', {
         'form': form,
         'categories': categories,
@@ -63,18 +61,18 @@ def transaction(request):
             amount = request.POST.get('amount')
 
             try:
-                ledger = Ledger.objects.get(start_date__lte=transaction_date, end_date__gte=transaction_date)
+                ledger = Ledger.objects.get(start_date__lte=transaction_date,
+                                            end_date__gte=transaction_date)
                 if ledger.is_balance_sheet:
-                    error_message = ('No se pueden agregar transacciones a un libro mayor con cierre contable.')
+                    error_message = ('No se pueden agregar transacciones a un '
+                                     'libro mayor con cierre contable.')
                 else:
                     new_transaction = form.save(commit=False)
 
                     if transaction_type == 'debit':
                         new_transaction.transaction_debit_amount = amount
-                        new_transaction.transaction_credit_amount = 0
                     elif transaction_type == 'credit':
                         new_transaction.transaction_credit_amount = amount
-                        new_transaction.transaction_debit_amount = 0
 
                     new_transaction.ledger = ledger
                     new_transaction.save()
@@ -82,9 +80,11 @@ def transaction(request):
                     return redirect('transaction')
             except Ledger.DoesNotExist:
                 error_message = (
-                    'No se puede agregar la transacción porque no existe un libro mayor para esa fecha.')
+                    'No se puede agregar la transacción porque no '
+                    'existe un libro mayor para esa fecha.')
         else:
-            error_message = ('Formulario no válido. Por favor, verifica los campos.')
+            error_message = ('Formulario no válido. Por favor, verifica los '
+                             'campos.')
 
     else:
         form = TransactionForm()
@@ -101,6 +101,7 @@ def transaction(request):
 
     return render(request, 'transaction.html', context)
 
+
 def ledgers(request):
     if request.method == 'POST':
         form = LedgerForm(request.POST)
@@ -116,15 +117,47 @@ def ledgers(request):
         'ledgers': Ledger.objects.all().order_by('-start_date'),
     })
 
+
 def ledger(request, ledger_id):
     ledger = get_object_or_404(Ledger, pk=ledger_id)
+    ordered_accounts = get_account_order_by_category()
+    transactions = Transaction.objects.filter(ledger=ledger).order_by(
+        'transaction_date')
 
-    ordered_categories = get_account_order_by_category()
+    account_totals = {}
+    for transaction in transactions:
+        account_name = transaction.account.name
+        if account_name not in account_totals:
+            account_totals[account_name] = {
+                'debit_total': transaction.transaction_debit_amount,
+                'credit_total': transaction.transaction_credit_amount,
+            }
+        else:
+            account_totals[account_name][
+                'debit_total'] += transaction.transaction_debit_amount
+            account_totals[account_name][
+                'credit_total'] += transaction.transaction_credit_amount
 
-    transactions = Transaction.objects.filter(ledger=ledger).order_by('transaction_date')
+    # mostrar el total de cada cuenta en terminal
+    for account_name, totals in account_totals.items():
+        print(f'{account_name} - {totals["debit_total"]}')
+
+    # actualizar el balance de cada cuenta
+    for account_name, totals in account_totals.items():
+        if totals['debit_total'] > totals['credit_total']:
+            account = Account.objects.get(name=account_name)
+            account.balance = totals['debit_total'] - totals['credit_total']
+            account.balance_type = 'debit'
+            account.save()
+        elif totals['credit_total'] > totals['debit_total']:
+            account = Account.objects.get(name=account_name)
+            account.balance = totals['credit_total'] - totals['debit_total']
+            account.balance_type = 'credit'
+            account.save()
 
     return render(request, 'ledger.html', {
         'ledger': ledger,
-        'categorized_accounts': ordered_categories,
+        'categorized_accounts': ordered_accounts,
         'transactions': transactions,
+        'account_totals': account_totals,
     })
