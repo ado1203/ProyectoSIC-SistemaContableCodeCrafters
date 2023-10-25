1aa5f0f216abff084c7df680893d39609780423d
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-10-24 23:13:33 -0600

    Agregado: Refactor modelos y mejorado registro de transacciones

--- general_ledger/views.py
+++ general_ledger/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import AccountForm, TransactionForm
-from .models import Account, Category, Transaction
+from .models import Account, Category, Transaction, Ledger
 
 
 def account(request):
@@ -31,8 +31,6 @@ def create_account(request):
         form = AccountForm(request.POST)
         if form.is_valid():
             new_account = form.save(commit=False)
-            new_account.balance = 0
-            new_account.catalog_id = 1
             new_account.save()
             return redirect('account')
     else:
@@ -46,28 +44,59 @@ def create_account(request):
 
 
 def transaction(request):
+    error_message = None
+
     if request.method == 'POST':
         form = TransactionForm(request.POST)
         if form.is_valid():
-            new_transaction = form.save(commit=False)
+            transaction_date = form.cleaned_data['transaction_date']
             transaction_type = form.cleaned_data['transaction_type']
             amount = request.POST.get('amount')
 
-            if transaction_type == 'debit':
-                new_transaction.transaction_debit_amount = amount
-            elif transaction_type == 'credit':
-                new_transaction.transaction_credit_amount = amount
+            try:
+                ledger = Ledger.objects.get(start_date__lte=transaction_date,
+                                            end_date__gte=transaction_date)
+                if ledger.is_balance_sheet:
+                    error_message = ('No se pueden agregar transacciones a un '
+                                     'libro mayor con cierre contable.')
+                else:
+                    new_transaction = form.save(commit=False)
 
-            new_transaction.ledger_id = 1
-            new_transaction.save()
-            return redirect('transaction')
+                    if transaction_type == 'debit':
+                        new_transaction.transaction_debit_amount = amount
+                        new_transaction.transaction_credit_amount = 0
+                    elif transaction_type == 'credit':
+                        new_transaction.transaction_credit_amount = amount
+                        new_transaction.transaction_debit_amount = 0
+
+                    new_transaction.ledger = ledger
+                    new_transaction.save()
+
+                    return redirect('transaction')
+            except Ledger.DoesNotExist:
+                error_message = (
+                    'No se puede agregar la transacción porque no '
+                    'existe un libro mayor para esa fecha.')
+        else:
+            error_message = ('Formulario no válido. Por favor, verifica los '
+                             'campos.')
+
     else:
         form = TransactionForm()
 
     accounts = Account.objects.all()
     transactions = Transaction.objects.all().order_by('-transaction_date')
-    return render(request, 'transaction.html', {
+
+    context = {
         'form': form,
         'accounts': accounts,
         'transactions': transactions,
-    })
+        'error_message': error_message,
+    }
+
+    return render(request, 'transaction.html', context)
+
+
+def ledger(request):
+    transactions = Transaction.objects.all()
+    return render(request, 'ledger.html')
