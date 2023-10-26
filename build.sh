a7f60ec88735ed5fadf9e50b1f76f3a69533f2e2
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-10-26 11:14:41 -0600

    Add: Corregir migraciones

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
-# Crear superusuario inicialmente
-python manage.py shell < create_superuser.py
-
 # Realizar migraciones
 python manage.py migrate
+
+# Crear superusuario inicialmente
+python manage.py shell < create_superuser.py
 
 # Cargar datos desde archivos JSON
 python manage.py loaddata fixtures/category_fixture.json
