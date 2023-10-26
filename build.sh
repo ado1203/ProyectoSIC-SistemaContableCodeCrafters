38b4a4265876922c1652d205506990858bf6cf8f
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-10-26 11:09:06 -0600

    Add: Corregir requirements de nuevo

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # exit on error
 set -o errexit
 
-# pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Crear superusuario inicialmente
 python manage.py shell < create_superuser.py
